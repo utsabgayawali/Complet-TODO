faf5c793dd5377fa4555a4dca92915c017a8ba96
Utsab Gayawali <utsavgayawali@gmail.com>
2025-11-27 18:20:53 +0545

    2nd commit

---
+++ home/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path,include
+from home import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('home/',views.home,name='home'),
+    path('clone/',views.clone,name='clone'),
+    path('home3/',views.home3,name='home3'),
+    path('home4/',views.home4,name='home4'),
+   
+]
--- home/views.py
+++ home/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse
 
-# Create your views here.
+
+
+def  home(request):
+    return  HttpResponse(" url routing for github repo home")
+def  clone(request):
+    return  HttpResponse(" url routing for github repo home2")
+def  home3(request):
+    return  HttpResponse(" url routing for github repo home3")
+def  home4(request):
+    return  HttpResponse(" url routing for github repo home4")
