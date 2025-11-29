fefe09f54b7aabc3e620b4e811b8a463313923fb
Utsab Gayawali <utsavgayawali@gmail.com>
2025-11-29 16:44:46 +0545

    1st commit

--- home/urls.py
+++ home/urls.py
@@ -5,9 +5,6 @@ from home import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('home/',views.home,name='home'),
-    path('clone/',views.clone,name='clone'),
-    path('home3/',views.home3,name='home3'),
-    path('home4/',views.home4,name='home4'),
-    path('home5/',views.home4,name='home5'),
+    
    
 ]
--- home/views.py
+++ home/views.py
@@ -2,13 +2,5 @@ from django.shortcuts import render,HttpResponse
 
 
 
-def  home(request):
-    return  HttpResponse(" url routing for github repo home")
-def  clone(request):
-    return  HttpResponse(" url routing for github repo home2")
-def  home3(request):
-    return  HttpResponse(" url routing for github repo home3")
-def  home4(request):
-    return  HttpResponse(" url routing for github repo home4")
-def  home5(request):
-    return  HttpResponse(" url routing for github repo home4")
+def home(request):
+    return render(request,'home.html')
