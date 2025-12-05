6fa9a98898de31f001b18004429d7a80092bd19a
Utsab Gayawali <utsavgayawali@gmail.com>
2025-12-05 17:31:40 +0545

    3rd

--- home/views.py
+++ home/views.py
@@ -5,5 +5,5 @@ from django.shortcuts import render,HttpResponse
 def home(request):
     return render(request,'home.html')
 
-def content(request):
-    return render(request,'content.html')
+def task(request):
+    return render(request,'task.html')
