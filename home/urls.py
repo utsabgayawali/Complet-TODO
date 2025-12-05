43ceb5aee0e32c77274e35f5b3a0e5a0ce336983
Utsab Gayawali <utsavgayawali@gmail.com>
2025-12-05 17:25:24 +0545

    1st

--- home/urls.py
+++ home/urls.py
@@ -5,6 +5,7 @@ from home import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('home/',views.home,name='home'),
+    path('content/',views.content,name='content'),
     
    
 ]
--- home/views.py
+++ home/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render,HttpResponse
 
 def home(request):
     return render(request,'home.html')
+
+def content(request):
+    return render(request,'content.html')
