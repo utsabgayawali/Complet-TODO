a5a5c54cf100a3fd30894f2e862b96c3e44683f7
Utsab Gayawali <utsavgayawali@gmail.com>
2025-12-12 17:47:55 +0545

    1st commit

--- home/views.py
+++ home/views.py
@@ -4,6 +4,11 @@ from .models import Todo
 
 
 def home(request):
+    if request.method == 'POST':
+        task_name= request.POST.get('task_name')
+        description = request.POST.get('description')
+        todo= Todo(task_name=task_name,description=description)
+        todo.save()
     return render(request,'home.html')
 
 def task(request):
