5cdba9420da92e9e8ecb344ec7d941830dcc6366
Utsab Gayawali <utsavgayawali@gmail.com>
2025-12-13 15:57:29 +0545

    1st

--- home/views.py
+++ home/views.py
@@ -20,8 +20,5 @@ def task(request):
 
 def delete(request,task_id):
     task = get_object_or_404(Todo, pk=task_id)
-    if request.method == 'POST':
-        task.delete()
-        return redirect('task')
-    else:
-        return render(request,'task.html')
+    task.delete()
+    return redirect('task')
